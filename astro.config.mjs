337907e1484b1ae52ab4e8892b25d41a0cb24999
Tropical18 <josedavid_irias@yahoo.com>
2026-07-20 16:34:42 -0600

    Configurar GitHub Pages para publicar la tienda

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://tropical18.github.io",
+  base: "/la-pulperia-catracha",
   vite: {
     plugins: [tailwindcss()]
   }
